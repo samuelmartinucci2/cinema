878f4d662a289fff8a57bfe3975df21d6814f813
Samel Martinucci <samuelmartinucci@gmail.com>
2015-04-02 14:02:06 -0300

    Initial commit

--- app/assets/javascripts/movie_ctrl.js
+++ app/assets/javascripts/movie_ctrl.js
@@ -27,4 +27,15 @@ cinema.controller("cinemaController", function ($scope) {
             "thumb": "http://upload.wikimedia.org/wikipedia/en/6/66/Transformers07.jpg"
         }
     ];
+
+    $scope.currMovie = null;
+    $scope.getMovieById = function (id) {
+        var movies = $scope.movies;
+        for (var i = 0; i < movies.length; i++) {
+            var movie = $scope.movies[i];
+            if (movie.id == id) {
+                $scope.currMovie = movie;
+            }
+        }
+    };
 });
